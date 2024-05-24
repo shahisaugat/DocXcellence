da70eacd33a6e544caf61dbf8aa1a1dfa314f16b
Saugat Shahi Thakuri <devops.shahi@gmail.com>
2024-05-24 17:57:09 +0545

    Intent Setup for Splash Activity

--- app/src/main/java/com/example/docxcellence/SplashActivity.kt
+++ app/src/main/java/com/example/docxcellence/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.docxcellence
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,14 +9,20 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.docxcellence.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var splashBinding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        splashBinding.getStartedBtn.setOnClickListener {
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
