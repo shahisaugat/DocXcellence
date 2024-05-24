1b05490dba93d815d77ffd64a7f26d840cc81013
Saugat Shahi Thakuri <devops.shahi@gmail.com>
2024-05-24 18:21:00 +0545

    Google Sign In Backend Integrated

--- app/src/main/java/com/example/docxcellence/LoginActivity.kt
+++ app/src/main/java/com/example/docxcellence/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.example.docxcellence
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.docxcellence.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginBinding: ActivityLoginBinding
@@ -14,7 +21,7 @@ class LoginActivity : AppCompatActivity() {
         private const val  RC_SIGN_IN = 120
     }
 
-    private lateinit var mAuth: FirebaseAuth
+    private var mAuth = FirebaseAuth.getInstance()
     private lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +33,23 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        loginBinding.backBtn.setOnClickListener {
-            goBack()
-        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
         loginBinding.sendOtpBtn.setOnClickListener {
             sendOTP()
+        }
+
+        loginBinding.signInGoogleBtn.setOnClickListener {
+            signInWithGoogle()
+        }
+
+        loginBinding.backBtn.setOnClickListener {
+            goBack()
         }
     }
     private fun sendOTP() {
@@ -40,7 +59,43 @@ class LoginActivity : AppCompatActivity() {
         val intent = Intent(this@LoginActivity, OTPActivity::class.java)
         startActivity(intent)
     }
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if (task.isSuccessful) {
+                try {
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("SignInActivity", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    Log.w("SignInActivity", "Google sign in failed!", e)
+                }
+            } else {
+                Log.w("SignInActivity", exception.toString())
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("SignInActivity", "signInWithCredential:success")
+                    val intent = Intent(this, DashboardActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Log.w("SignInActivity", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
     private fun goBack() {
         val intent = Intent(this@LoginActivity, SplashActivity::class.java)
         startActivity(intent)
