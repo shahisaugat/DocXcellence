d43cc60ef1714f06e8a8c385af0550f304f76aff
Saugat Shahi Thakuri <devops.shahi@gmail.com>
2024-05-24 17:42:32 +0545

    Login Activity Frontend

--- app/src/main/java/com/example/docxcellence/LoginActivity.kt
+++ app/src/main/java/com/example/docxcellence/LoginActivity.kt
@@ -5,13 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.docxcellence.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
