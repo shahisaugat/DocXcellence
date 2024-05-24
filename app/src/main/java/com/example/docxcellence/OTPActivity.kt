fcab15bca7823ec3626cb9e1cc8d90087248e1af
Saugat Shahi Thakuri <devops.shahi@gmail.com>
2024-05-24 18:07:59 +0545

    Intent Setup for OTP Activity

--- app/src/main/java/com/example/docxcellence/OTPActivity.kt
+++ app/src/main/java/com/example/docxcellence/OTPActivity.kt
@@ -1,20 +1,40 @@
 package com.example.docxcellence
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.docxcellence.databinding.ActivityOtpBinding
 
 class OTPActivity : AppCompatActivity() {
+    private lateinit var otpBinding: ActivityOtpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_otpactivity)
+        otpBinding = ActivityOtpBinding.inflate(layoutInflater)
+        setContentView(otpBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        otpBinding.backBtn.setOnClickListener {
+            goBack()
+        }
+        otpBinding.verifyOtpBtn.setOnClickListener {
+            verifyOTPCode()
+            // App might crash as no functionality has been assigned!
+        }
+    }
+    private fun goBack() {
+        val intent = Intent(this@OTPActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun verifyOTPCode() {
+        // Firebase Implementation here
     }
 }
