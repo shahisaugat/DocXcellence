661d5e44839c84e2916483df7bc27e2ce3883404
Sushant <susantmahat2@gmail.com>
2024-06-01 09:36:24 +0545

    changes in xml file named as Image Switzer

--- app/src/main/java/com/example/docxcellence/DashboardActivity.kt
+++ app/src/main/java/com/example/docxcellence/DashboardActivity.kt
@@ -1,6 +1,12 @@
 package com.example.docxcellence
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageSwitcher
+import android.widget.ImageView
+import android.widget.ViewSwitcher
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,11 +23,15 @@ class DashboardActivity : AppCompatActivity() {
         enableEdgeToEdge()
         dashboardBinding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(dashboardBinding.root)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
         val currentUser = mAuth.currentUser
         Glide.with(this).load(currentUser?.photoUrl).into(dashboardBinding.profilePicture)
 
@@ -29,4 +39,8 @@ class DashboardActivity : AppCompatActivity() {
         dashboardBinding.bottomNavigationView.menu.getItem(2).isEnabled = false
         dashboardBinding.textView4.text=currentUser?.displayName.toString()
     }
+
+
 }
+
+
